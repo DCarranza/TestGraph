9b422be201afce8c836db5f70e9c16ddc8b275b7
Diego Carranza <Diego@wr-130-64-196-192.medford.tufts.edu>
2014-11-16 15:51:31 -0500

    Initial Commit

---
+++ TestGraph/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGraph
+//
+//  Created by Diego Carranza on 11/16/14.
+//  Copyright (c) 2014 tufts.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
